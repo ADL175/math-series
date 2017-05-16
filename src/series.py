3297286fde33b95e7634f8fdfd6db067fcef1aa4
Zachary Diehl <zachary.diehl@gmail.com>
2017-05-16 14:33:29 -0700

    finished writing lucas function and all tests passed

--- src/series.py
+++ src/series.py
@@ -2,3 +2,10 @@ def fib_func(n):
     if n<2:
         return n
     return fib_func(n-2) + fib_func(n-1)
+
+def lucas_func(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    return lucas_func(n -1) + lucas_func(n -2)
